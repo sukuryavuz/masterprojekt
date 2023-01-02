79bc4507d897deda6a5298c26acf7179572cc02b
sukuryavuz <sukur.yavuz@hotmail.com>
2023-01-02 19:44:03 +0100

    Initial commit

---
+++ src/main/java/com/fhcampuswien/masterprojekt/MasterprojektApplication.java
@@ -0,0 +1,13 @@
+package com.fhcampuswien.masterprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterprojektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fhcampuswien/masterprojekt/MasterprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fhcampuswien.masterprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterprojektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
